d0fc58b88f304f00e809e4f0c6f107a76fc86b77
Benjamin Bastian <benjamin.bastian@gmail.com>
2011-10-23 20:00:49 -0700

    Changed function names to camel-case and changed foldl (quadratic time) to foldr (linear time). The latter probably won't make any difference, but it won't hurt.

--- SimpleHTTP.hs
+++ SimpleHTTP.hs
@@ -14,22 +14,22 @@ data Option = Auth String String
 instance Show Option where
     show (Auth u p) = u ++ ":" ++ p
 
-parse_option :: Option -> [CurlOption]
-parse_option (Auth u p) = [CurlHttpAuth [HttpAuthBasic], CurlUserPwd $ show (Auth u p)]
-parse_option (PostFields fs) = [CurlPostFields fs]
+parseOption :: Option -> [CurlOption]
+parseOption (Auth u p) = [CurlHttpAuth [HttpAuthBasic], CurlUserPwd $ show (Auth u p)]
+parseOption (PostFields fs) = [CurlPostFields fs]
 
-parse_method :: Method -> [CurlOption]
-parse_method (Other a) = [CurlCustomRequest a]
-parse_method GET = method_GET
-parse_method POST = method_POST
-parse_method HEAD = method_HEAD
-parse_method method = [CurlCustomRequest $ show method]
+parseMethod :: Method -> [CurlOption]
+parseMethod (Other a) = [CurlCustomRequest a]
+parseMethod GET = method_GET
+parseMethod POST = method_POST
+parseMethod HEAD = method_HEAD
+parseMethod method = [CurlCustomRequest $ show method]
 
-do_request :: Request -> IO Response
-do_request (Request url method options) = do
+doRequest :: Request -> IO Response
+doRequest (Request url method options) = do
     curl <- initialize
-    response <- do_curl_ curl url (foldl (++) (parse_method method) (map parse_option options)) :: IO CurlResponse
+    response <- do_curl_ curl url (foldr (++) (parseMethod method) (map parseOption options)) :: IO CurlResponse
     return $ Response (respBody response) (respStatus response) :: IO Response
 
 get_url :: Url -> IO Response
-get_url u = do_request (Request u GET [])
+get_url u = doRequest (Request u GET [])
